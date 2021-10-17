5a68848cd31d4aa064f53ad46db5406e96330475
Dumar Yeksel Basto Moreno - Ceiba Software <dumar.basto@ceiba.com.co>
2021-10-17 13:28:06 -0500

    remove .idea

---
+++ app/src/main/java/com/plcoding/jetpackcomposepokedex/GetColorDominantTransformation.kt
@@ -0,0 +1,29 @@
+package com.plcoding.jetpackcomposepokedex
+
+import android.graphics.Bitmap
+import androidx.compose.ui.graphics.Color
+import androidx.palette.graphics.Palette
+import coil.bitmap.BitmapPool
+import coil.size.Size
+import coil.transform.Transformation
+
+class GetColorDominantTransformation(private val onFinish: (Color) -> Unit) : Transformation {
+
+    override fun key(): String = GetColorDominantTransformation::class.java.name
+
+    override suspend fun transform(pool: BitmapPool, input: Bitmap, size: Size): Bitmap {
+        Palette.from(input).generate { palette ->
+            palette?.dominantSwatch?.rgb?.let { colorValue ->
+                onFinish(Color(colorValue))
+            }
+        }
+        return input
+    }
+
+    override fun equals(other: Any?) = other is GetColorDominantTransformation
+
+    override fun hashCode() = javaClass.hashCode()
+
+    override fun toString() = "GetColorDominantTransformation()"
+
+}
---
+++ app/src/main/java/com/plcoding/jetpackcomposepokedex/data/models/PokedexListEntry.kt
@@ -0,0 +1,9 @@
+package com.plcoding.jetpackcomposepokedex.data.models
+
+import androidx.compose.ui.graphics.Color
+
+data class PokedexListEntry(
+    val pokemonName: String,
+    val imageUrl: String,
+    val number: Int
+)
